b1d3150d735cb628016410d7d7d428339c90a7d8
joan-w <wamuyujoan4@gmail.com>
2020-02-23 12:08:13 +0300

    Adding a functionality that toggle the dev id when clicked

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
         $("#design-show").toggle("slow");
         $("#design-hide").toggle(1000);
     });
-    // $("#dev").click(function() {
-    //     $("#dev-show").toggle("slow");
-    //     $("#dev-hide").toggle(1000);
-    // });
+    $("#dev").click(function() {
+        $("#dev-show").toggle("slow");
+        $("#dev-hide").toggle(1000);
+    });
     // $("#prod").click(function() {
     //     $("#prod-show").toggle("slow");
     //     $("#prod-hide").toggle(1000);
