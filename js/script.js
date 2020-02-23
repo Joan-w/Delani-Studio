a43b25b86ba3b2c9e77e9b19330b7361987a1ffb
joan-w <wamuyujoan4@gmail.com>
2020-02-23 12:07:36 +0300

    Adding a functionality that toggle the design id when clicked

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-    $(".togle p").hide();
-    $("#design").click(function(){
-        $("#design p").toggle();
-        $("#design img").toggle();
+    $("#design").click(function() {
+        $("#design-show").toggle("slow");
+        $("#design-hide").toggle(1000);
     });
-    $("#dev").click(function(){
-        $("#dev p").toggle();
-        $("#dev img").toggle();
-    });
-    $("#prod").click(function(){
-        $("#prod p").toggle();
-        $("#prod img").toggle();
-    });
-});
+    // $("#dev").click(function() {
+    //     $("#dev-show").toggle("slow");
+    //     $("#dev-hide").toggle(1000);
+    // });
+    // $("#prod").click(function() {
+    //     $("#prod-show").toggle("slow");
+    //     $("#prod-hide").toggle(1000);
+    // });
+});    
